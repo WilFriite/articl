b048231e2a32406b6d31143988cfdfb48c7f323c
WilFriite <jnguessan2003@gmail.com>
2025-10-14 11:19:49 +0200

    Initial commit

---
+++ inertia/pages/home.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+  import { Head } from '@inertiajs/vue3'
+  import { ref } from 'vue';
+  import { Button } from '~/components/ui/button';
+
+  const counter = ref(0);
+
+  const increment = () => {
+    counter.value++;
+  }
+</script>
+
+<template>
+  <Head title="Homepage" />
+
+  <Button @click="increment">
+    Click me
+  </Button>
+  <p>The count is: {{ counter }}</p>
+</template>
---
+++ vite.config.ts
@@ -0,0 +1,28 @@
+import { getDirname } from "@adonisjs/core/helpers";
+import inertia from "@adonisjs/inertia/client";
+import adonisjs from "@adonisjs/vite/client";
+import tailwindcss from "@tailwindcss/vite";
+import vue from "@vitejs/plugin-vue";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  plugins: [
+    inertia({ ssr: { enabled: false } }),
+    vue(),
+    adonisjs({
+      entrypoints: ["inertia/app/app.ts"],
+      reload: ["resources/views/**/*.edge"],
+    }),
+    tailwindcss(),
+  ],
+
+  /**
+   * Define aliases for importing modules from
+   * your frontend code
+   */
+  resolve: {
+    alias: {
+      "~/": `${getDirname(import.meta.url)}/inertia/`,
+    },
+  },
+});
