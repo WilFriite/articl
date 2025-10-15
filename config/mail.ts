0544c5dbd73a852aa19839d1817a380fe495791a
WilFriite <jnguessan2003@gmail.com>
2025-10-15 16:20:08 +0200

    Refactor mail configuration to remove SMTP settings and clean up resend tags

--- config/mail.ts
+++ config/mail.ts
@@ -10,28 +10,9 @@ const mailConfig = defineConfig({
    * options.
    */
   mailers: {
-    smtp: transports.smtp({
-      host: env.get("SMTP_HOST"),
-      port: env.get("SMTP_PORT"),
-      /**
-       * Uncomment the auth block if your SMTP
-       * server needs authentication
-       */
-      /* auth: {
-        type: 'login',
-        user: env.get('SMTP_USERNAME'),
-        pass: env.get('SMTP_PASSWORD'),
-      }, */
-    }),
     resend: transports.resend({
       baseUrl: "https://api.resend.com",
       key: env.get("RESEND_API_KEY"),
-      tags: [
-        {
-          name: "category",
-          value: "confirm_email",
-        },
-      ],
     }),
   },
 });
